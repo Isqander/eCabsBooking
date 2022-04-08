5ef992194323dbc41c33931ad8cdc2f3a7d06b6a
isqander <isqander@gmail.com>
2022-04-08 16:25:29 +0300

    Boot init

---
+++ src/main/java/com/example/ecabsbooking/ECabsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecabsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECabsBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECabsBookingApplication.class, args);
+    }
+
+}
